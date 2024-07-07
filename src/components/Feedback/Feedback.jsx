990a35298e688537b46e1b6b86aeddb7be57d845
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-07-07 14:58:51 +0300

    add shadow feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,23 @@
-import css from './Feedback.module.css'
+import css from './Feedback.module.css';
 
 const Feedback = ({ feedback, totalFeedback, positivePercentage }) => {
+  const badPercentage = totalFeedback ? (feedback.bad / totalFeedback) * 100 : 0;
+  let positiveClass = "";
+  if (positivePercentage >= 66) {
+    positiveClass = css.pG;
+  } else if (positivePercentage >= 35 && positivePercentage <= 65) {
+    positiveClass = css.pN;
+  } else {
+    positiveClass = css.pB;
+  }
+
   return (
     <div className={css.div}>
       <p className={css.pG}>Good: {feedback.good}</p>
       <p className={css.pN}>Neutral: {feedback.neutral}</p>
       <p className={css.pB}>Bad: {feedback.bad}</p>
-      <p className={css.p}>Total Feedback: {totalFeedback}</p>
-      <p className={css.p}>Positive Feedback: {positivePercentage}%</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p className={css.p + ' ' + positiveClass}>Positive Feedback: {positivePercentage}%</p>
     </div>
   );
 };
